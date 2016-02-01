35dcd9ca2ca7b7d2ba3a1996c13eb3940792ad3f
周悦秋 <ethan@goodrain.com>
2016-02-01 11:03:05 +0800

    添加应用关联判断

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,8 +9,8 @@ RETRY=" -s --connect-timeout 3 --max-time 3  --retry 5 --retry-delay 0 --retry-m
 
 SERVICE_NAME=${SERVICE_NAME:-NONE}
 SERVICE_ID=${SERVICE_ID:-NONE}
-
-
+ELASTICSEARCH_HOST=${ELASTICSEARCH_HOST:-127.0.0.1}
+ELASTICSEARCH_PORT=${ELASTICSEARCH_PORT:-9200}
 
 # 初始化创建目录
 [ ! -d $CONFDIR ] && mkdir $CONFDIR
@@ -35,28 +35,37 @@ ucloud-bj-1)
   ZMQ_IP="10.3.1.2:9243";;
 esac
 
-#==========================================
+#========= processing config file ========
 
-oldIFS=$IFS  #定义一个变量为默认IFS
-IFS=,        #设置IFS为逗号
-
-for ser in $REVERSE_DEPEND_SERVICE
-do
-  service_name=`echo $ser| cut -d ':' -f 1`
-  service_id=`echo $ser| cut -d ':' -f 2`
-  cp /tmp/input.tpl /data/config/${service_name}_input.conf
-  sed -i -e "s/SERVICE_ID/$service_id/" \
-  -e "s/SERVICE_NAME/$service_name/" \
-  -e "s/ZMQ_IP/$ZMQ_IP/"
-  /data/config/${service_name}_input.conf
-done
+# input 
+if [ "$REVERSE_DEPEND_SERVICE" != "" ];then
+  oldIFS=$IFS  #定义一个变量为默认IFS
+  IFS=,        #设置IFS为逗号
+  for ser in $REVERSE_DEPEND_SERVICE
+  do
+    service_name=`echo $ser| cut -d ':' -f 1`
+    service_id=`echo $ser| cut -d ':' -f 2`
+    cp /tmp/input.tpl /data/config/${service_name}_input.conf
+    sed -i -e "s/SERVICE_ID/$service_id/" \
+    -e "s/SERVICE_NAME/$service_name/" \
+    -e "s/ZMQ_IP/$ZMQ_IP/"
+    /data/config/${service_name}_input.conf
+  done
+else
+cat >> $CONFDIR/default_input.conf << EOF
+input {
+  stdin {
+  }
+}
+EOF
+fi
 
 IFS=$oldIFS  #还原IFS为默认值
 
-
-
-
-
+# output
+sed -i -e "s/ELASTICSEARCH_HOST/$ELASTICSEARCH_HOST/" \
+       -e "s/ELASTICSEARCH_PORT/$ELASTICSEARCH_PORT/" \
+       ${CONFDIR}/${LSCONFIG}
 
 #===========================================
 
